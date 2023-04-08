d072748fd0b460e7f99d663d8143a1686d0545c3
Raja <vickyclans3@gmail.com>
2023-04-08 11:00:50 +0530

    looped the case statement in main file

--- src/com/maths/java/Main.java
+++ src/com/maths/java/Main.java
@@ -23,78 +23,80 @@ public class Main {
 			// Database connection
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Employee_details", "root", "Root@2709");
-			System.out.println("1. Register");
-			System.out.println("2. Login");
-			System.out.print("Enter your choice: ");
-			int choice = in.nextInt();
+			while(true) {
+				System.out.println("\n1. Register");
+				System.out.println("2. Login");
+				System.out.print("Enter your choice: ");
+				int choice = in.nextInt();
 
-			switch (choice) {
-			case 1:
+				switch (choice) {
+				case 1:
 
-				System.out.println("\nSign up");
-				userName = in.nextLine();
-				do {
-					System.out.print("Enter username:");
+					System.out.println("\nSign up");
 					userName = in.nextLine();
-					userName = userName.toLowerCase();
-				}while(!ValidityChecker.isUnameValid(userName));
-				
-				do {
-					System.out.print("Enter password:");
-					password = in.nextLine();
-				}while(!ValidityChecker.isPassValid(password));
-				
-				do {
-					System.out.print("Enter email:");
-					email = in.nextLine();
-				}while(!ValidityChecker.isEmailValid(email));
-				
-				do {
-					System.out.print("Enter dob:");
-					dob = in.nextLine();
-				}while(!ValidityChecker.isDobValid(dob));
+					do {
+						System.out.print("Enter username:");
+						userName = in.nextLine();
+						userName = userName.toLowerCase();
+					}while(!ValidityChecker.isUnameValid(userName));
+					
+					do {
+						System.out.print("Enter password:");
+						password = in.nextLine();
+					}while(!ValidityChecker.isPassValid(password));
+					
+					do {
+						System.out.print("Enter email:");
+						email = in.nextLine();
+					}while(!ValidityChecker.isEmailValid(email));
+					
+					do {
+						System.out.print("Enter dob:");
+						dob = in.nextLine();
+					}while(!ValidityChecker.isDobValid(dob));
 
-				Register rg = new Register(userName, password, email, dob);
-				rg.register();
-				break;
+					Register rg = new Register(userName, password, email, dob);
+					rg.register();
+					break;
 
-			case 2:
-				boolean isLogged = false;
-				int loginFailCount = 0;
-				String resetChoice = null;
-				userName = in.nextLine();
-				while (!isLogged && loginFailCount < 4) {
-
-					System.out.println("\nLogin");
-					System.out.print("Enter username: ");
+				case 2:
+					boolean isLogged = false;
+					int loginFailCount = 0;
+					String resetChoice = null;
 					userName = in.nextLine();
-					userName = userName.toLowerCase();
-					System.out.println("Enter password: ");
-					password = in.nextLine();
+					while (!isLogged && loginFailCount < 4) {
 
-					Login lg = new Login(userName, password);
-					if (ValidityChecker.isUnameValid(userName)) {
-						if (ValidityChecker.isPassValid(password)) {
-							isLogged = lg.login();
-							if (!isLogged) {
-								loginFailCount++;
-							}
-							if (loginFailCount >= 2) {
-								System.out.print("\nDo you want to reset password(y/n): ");
-								resetChoice = in.nextLine();
-								if (resetChoice.equals("y")) {
-									PrepareEmail mail = new PrepareEmail(userName);
-									mail.mail();
-									loginFailCount = 0;
+						System.out.println("\nLogin");
+						System.out.print("Enter username: ");
+						userName = in.nextLine();
+						userName = userName.toLowerCase();
+						System.out.println("Enter password: ");
+						password = in.nextLine();
+
+						Login lg = new Login(userName, password);
+						if (ValidityChecker.isUnameValid(userName)) {
+							if (ValidityChecker.isPassValid(password)) {
+								isLogged = lg.login();
+								if (!isLogged) {
+									loginFailCount++;
+								}
+								if (loginFailCount >= 2) {
+									System.out.print("\nDo you want to reset password(y/n): ");
+									resetChoice = in.nextLine();
+									if (resetChoice.equals("y")) {
+										PrepareEmail mail = new PrepareEmail(userName);
+										mail.mail();
+										loginFailCount = 0;
+									}
 								}
 							}
 						}
 					}
+					break;
+				default:
+					System.out.println("Enter a valid choice");
+					break;
 				}
-				break;
-			default:
-				System.out.println("Enter a valid choice");
-				break;
 			}
 		} catch (Exception e) {
 			System.out.println(e);
